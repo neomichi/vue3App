c74b0ca497f99287c10f833b45521e83e59ac650
Николай <tneomichi@gmail.com>
2021-01-09 14:15:43 +0500

    init

---
+++ src/views/Auth.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>auth</h2>
+    </div>
+</template>
+<script lang="ts">
+
+import { defineComponent,ref } from 'vue';
+export default defineComponent({ 
+  setup() {
+    function onInput(ev:any) {
+      console.log(`${ev.target.value}`)
+    }
+    return {onInput}
+
+  }
+})
+
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">  
+    <div>
+      {{gg}}
+      </div>  
+      <div class="center">
+        <inputForm :onInput="onInp" :value="value"></inputForm>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import inputForm from "../components/inputForm.vue"
+import { defineComponent,ref } from 'vue';
+
+
+export default defineComponent({
+  name: 'Home',
+  components:{
+    inputForm
+  },
+  setup() {   
+  const gg=ref('home')
+    return {gg}
+  }
+  
+});
+</script>
+<style>
+div.center {
+  display: flex;
+  justify-content: center;
+}
+</style>
