9e6f67b9655291d483e206cd6ce9aa5183045bc5
Николай <tneomichi@gmail.com>
2021-01-09 19:27:14 +0500

    fix footer to bottom

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<div class="template">
+  <Header class="header"></Header>
+  <router-view class="main"/>
+  <Footer class="footer"></Footer>
   </div>
-  <router-view/>
 </template>
 
+<script lang="ts">
+
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+import { defineComponent,ref } from 'vue';
+export default defineComponent({
+components: {
+  Header,Footer
+}
+})
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "./assets/scss/main";
 </style>
+
