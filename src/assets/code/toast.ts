f38b8f06edbbffbdfe69d3f8562bd7ee5aa0846a
Николай <tneomichi@gmail.com>
2021-01-13 15:34:36 +0500

    add toast

---
+++ src/assets/code/toast.ts
@@ -0,0 +1,24 @@
+import Toastr from 'toastr2';
+import 'toastr2/dist/toastr.min.css';
+
+export class Toast {
+
+    public static readonly toastr:any = new Toastr();
+
+    public static info(text:string){
+       this.toastr.info(text)
+    }
+
+    public static success(text:string) {
+        this.toastr.success(text)
+    }
+
+    public static error(text:string) {
+        this.toastr.error(text)
+    }
+
+    public static warning(text:string) {
+        this.toastr.error(text)
+    }    
+
+}
