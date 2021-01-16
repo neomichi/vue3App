4302255ef26112293b7e185ac6afa572678d10e4
Николай <tneomichi@gmail.com>
2021-01-16 09:47:49 +0500

    add static helper methods

--- src/assets/code/helper.ts
+++ src/assets/code/helper.ts
@@ -1,21 +1,21 @@
+import { User } from "./user";
 
 export class Helper {
-    static stringIsNullOrEmpty=(str:string|null)=>str===null || str===undefined || str.length===0
-    
+ public static stringIsNullOrEmpty = (str: string |undefined | null) =>
+    str === null || str === undefined || str.length === 0;
+
+    public static objectIsNullOrEmpty = <T extends Object>(obj: T) => {
+    return obj == null || Object.keys(obj).length == 0;
+  };
+  public static userRole = (role: string) => User.GetRoleFromString(role);
+
+  /// return value in obj[key].
+  public static objectKeyValue = function<T extends Object>(obj: T, str: string) {
+    let result: any;
+    for (const [key, value] of Object.entries(obj)) {
+      if (key === str) result = value;
+    }
+    return result;
+  };
+   
 }
-
-
-
-
-
-interface IAnimal {
-    name?: string;
-  }
-  
-let animal: Readonly<IAnimal>={}
-animal.name
-
-type ValidMember="callback"
-
-let v2: Record<string, any> = {v:"callback",c:"data"}
-
